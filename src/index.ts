1640b576694be44f0c0c9cec19dd2fea317b1002
Gunjan Datta <me@dattabase.com>
2021-09-16 08:33:08 -0400

    Added a global variable.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
+import * as Common from "./common";
 export * from "./common";
+import * as Dashboard from "./dashboard";
 export * from "./dashboard";
+import { ItemForm } from "./itemForm";
 export * from "./itemForm";
 
 /** Styling */
 import "./styles";
+
+/** Global Variable */
+window["DattaTable"] = {
+    Common, Dashboard, ItemForm
+}
