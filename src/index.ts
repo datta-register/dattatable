a7f5dce87782388a714b7040aa03b9cf99d0931b
Gunjan Datta <me@dattabase.com>
2023-11-22 11:30:22 -0500

    Updated to apply the theme automatically.

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,7 @@ import "./styles";
 /** Global Variable */
 import { DattaTable } from "./lib";
 window["DattaTable"] = DattaTable;
+
+/** Apply the Current Theme */
+import { ThemeManager } from "gd-sprest-bs";
+ThemeManager.load(true);
